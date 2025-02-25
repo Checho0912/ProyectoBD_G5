be489b0e2c540075c1055675160045b79c02c3b8
victorortiz <victorandresortiz@gmail.com>
2025-02-24 18:31:39 -0600

    Commit inicial con los archivos del proyecto creados.

---
+++ src/EasyITFrontend/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script >
+export default{
+  name:'Hello World',
+  data(){
+    return{
+      msg:'hola'
+    }
+  },
+  mounted(){
+    fetch("http://localhost:8080/api/messages/hello")
+    .then((response)=>response.text())
+    .then((data)=>{
+      
+      this.msg = data;
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+}
+
+</script>
+
+<template>
+  <h1>
+    {{msg}}
+  </h1>
+</template>
+
+
---
+++ src/main/java/com/proyecto/EasyIT/controller/MessageController.java
@@ -0,0 +1,15 @@
+package com.proyecto.EasyIT.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "full stack app con Springboot y Vue JS !";
+    }
+}
